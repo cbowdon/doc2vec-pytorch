6f654d98b0292f642a97d7cf4f54466af16c9cce
Chris Bowdon <chris.bowdon@polecat.com>
2019-04-23 11:47:34 +0100

    Reproducibility: fresh venv, requirements, setup
    Results re-run to confirm reproducibility. Minor bug in most_similar
    function fixed.

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+python3 -m venv venv &&
+    venv/bin/pip install --upgrade pip &&
+    venv/bin/pip install -r requirements &&
+    venv/bin/python3 -m spacy download en_core_web_sm

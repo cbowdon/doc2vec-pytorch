cf063d80334fc6b2f4785f0c0db86207bf7a3402
Chris Bowdon <chris.bowdon@polecat.com>
2019-04-24 14:23:29 +0100

    Fix setup script errors - data package in reqs and typo in script

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 python3 -m venv venv &&
     venv/bin/pip install --upgrade pip &&
-    venv/bin/pip install -r requirements &&
+    venv/bin/pip install -r requirements.txt &&
     venv/bin/python3 -m spacy download en_core_web_sm
